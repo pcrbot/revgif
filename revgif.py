b181614239aaa071b2f508ab0f2ea1a99ac66e5f
Maximilian Wu <me@maxesisn.online>
2020-12-01 23:02:00 +0800

    add frame limit function

--- revgif.py
+++ revgif.py
@@ -16,6 +16,7 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9
            }
 fd = os.path.dirname(__file__)
 
+
 @sv.on_keyword("倒放")
 async def revgif(bot, ev):
     message_id = None
@@ -26,7 +27,7 @@ async def revgif(bot, ev):
         print(message_id)
         pre_message = await bot.get_msg(message_id=message_id)
         pre_raw_message = pre_message["message"]
-        print(pre_message, "\nwwww",pre_raw_message)
+        print(pre_message, "\nwwww", pre_raw_message)
         await match_revgif(bot, ev, custom=pre_raw_message)
     else:
         await match_revgif(bot, ev)
@@ -58,11 +59,14 @@ async def do_revgif(bot, ev, image_url):
     if len(sequence) == 1:
         print("并非GIF图片")
         return
+    if len(sequence) > 200:
+        await bot.finish(ev, "GIF帧数太多了，懒得倒放[CQ:face,id=13]")
+    await bot.send(ev, "ℹ正在翻转图片序列，请稍候")
     sequence.reverse()
-    gif_path=os.path.join(fd,f"{ev.user_id}.gif")
+    gif_path = os.path.join(fd, f"{ev.user_id}.gif")
     sequence[0].save(gif_path, save_all=True,
                      append_images=sequence[1:])
-    
+
     if os.path.exists(gif_path):
         await bot.send(ev, f"[CQ:image,file=file:///{gif_path}]")
         os.remove(gif_path)
